18d37dbdc658b31b10bee309acf5f3680a3242b8
qu2re1 <florarouzpsg@hotmail.com>
2024-05-11 14:45:43 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function reverseWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .join(" ");
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
+    }
+  }
+  return stack.join("");
 }
